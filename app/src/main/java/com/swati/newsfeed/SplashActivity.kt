0dff60a55f3deaa151fa0536b7395afb020070e4
Swatipriya <swati.priya@zopsmart.com>
2024-05-17 16:27:50 +0530

    ref #1 added list ui

--- app/src/main/java/com/swati/newsfeed/SplashActivity.kt
+++ app/src/main/java/com/swati/newsfeed/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.swati.newsfeed
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -7,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.swati.newsfeed.databinding.ActivitySplashBinding
+import com.swati.newsfeed.feature.NewListingActivity
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
@@ -19,6 +21,8 @@ class SplashActivity : AppCompatActivity() {
         Glide.with(this).load(R.drawable.splash).into(binding.ivSplashPage)
             .waitForLayout()
         Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, NewListingActivity::class.java))
+            this.finish()
         }, 3000)
     }
 }
